dcfe27313d8773521142ac4941a156b32009d060
jussi_hyde <j.99hyytiainen@gmail.com>
2022-10-06 00:26:31 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Todotable from './components/Todolist';
 
 function App() {
+
+  const [todo, setTodo] = useState({date: '', desc: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+    };
+  
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+    };
+  
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index))
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<header className="App-header">
+       <h1>Simple Todolist</h1>
       </header>
+      <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add" />
+      </form>
+      <Todotable todos={todos} delete={deleteTodo}/>
     </div>
   );
 }
